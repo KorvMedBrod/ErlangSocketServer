df7d1bd2e09ff7736bc018940b841c231b9c3a2c
Ludwig Björk <gusbjorklu@student.gu.se>
2014-11-23 13:32:51 +0100

    updates

--- socket.erl
+++ socket.erl
@@ -1,16 +1,14 @@
--author('L Bjork <gusbjorklu@student.gu.se>').
-
 -module(socket).
 
--export([listen/0,loop/1,gen_data/1]).
+-author('L Bjork <gusbjorklu@student.gu.se>').
+
+-export([start/0,loop/1,match_data/1]).
 
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 -define(Port,8080).
--define(caseOne,"GetRandomTweet").
--define(casetwo,"GetTweet").
 
 % Call echo:listen(Port) to start the service.
-listen() ->
+start() ->
   {ok, LSocket} = gen_tcp:listen(?Port, ?TCP_OPTIONS),
   spawn(fun() -> accept(LSocket) end).
 
@@ -24,25 +22,26 @@ accept(LSocket) ->
   gen_tcp:controlling_process(Socket, Pid),
   accept(LSocket).
 
-% Echo back whatever data we receive on Socket.
+% Takes the recived data as "Data" and sends it to the pattern macher
 loop(Socket) ->
     case gen_tcp:recv(Socket, 0) of
         {ok, Data} ->
-          io:format(Data),
-            gen_tcp:send(Socket, gen_data(Data)),
+          io:format("In data ~p~n", [Data]),
+          gen_tcp:send(Socket, match_data(Data)),
             loop(Socket);
         {error, closed} ->
             ok
     end.
 
-
+-define(caseOne, <<"GetRandomTweet">>).
+-define(caseTwo, <<"GetTweet">>).
 %The pattern macting is towards "Bit Strings"
-gen_data(<<"GetRandomTweet">>) ->
+match_data(?caseOne) ->
   io:format("found GetRandomTweet ~n"),
   "Here's a random tweet";
-gen_data(<<"GetTweet">>) ->
+match_data(?caseTwo) ->
   io:format("found GetTweet ~n"),
   "a tweet";
-gen_data(_) ->
+match_data(_) ->
   io:format("found no match ~n"),
   "no match".
