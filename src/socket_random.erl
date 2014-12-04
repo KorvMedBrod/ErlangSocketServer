10fc73a47be8e89b3d6adec1a8691c0db039ae21
Ludwig Björk <gusbjorklu@student.gu.se>
2014-12-04 19:09:15 +0100

    random hashtags

---
+++ src/socket_random.erl
@@ -0,0 +1,18 @@
+-module(socket_server).
+
+-author('L Bjork <gusbjorklu@student.gu.se>').
+
+-export([start/0]).
+
+start() ->
+  {ok, Pid} = riakc_pb_socket:start("127.0.0.1", 10017),
+  randomEntry(Pid,<<"hashtags">>).
+
+randomEntry(Pid, Bucket) ->
+  {ok, [{1, [R]}]} = mapred(Pid, Bucket),
+  List = dict:to_list(R),
+  randomize(List).
+
+randomize(List)->
+  Index = random:uniform(length(List)),
+  lists:nth(Index,List).
