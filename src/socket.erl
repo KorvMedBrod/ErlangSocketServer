eede7a02afe0f2cd4b186ef8da9c4c9cbe2364c1
Ludwig Björk <gusbjorklu@student.gu.se>
2014-12-04 15:16:22 +0100

    typo

--- src/socket.erl
+++ src/socket.erl
@@ -62,6 +62,6 @@ match_data(?caseTwo) ->
 match_data(?caseThree) ->
   "{#sananaman, #769idoneigdf, #levelapp, #g_bf, #pjt2014, #centralbabiro, #dw_avengers, #veloraindonesia, #stalkers, #ilhamitunc, #kyuhyun4thwin, #matilampu, #lampumerah, #khamoshiyan, #ikede, #5thcpccf, #tvtokyo, #midweekhappiness, #wts, #npask, #lastfm, #listas_zoo, #swlille, #abdullahabdulaziz, #bambam, #swlyon, #jsb3, #winitwednesday, #sgkilometromv, #swgiza, #btsthanh, #teog, #xiumin, #swamman, #kumbadjid, #pymesunidas, #bbau, #5sosarias, #gsb2014, #jackbam}";
 match_data(?caseFour) ->
-  socket_mapreduce:start().
+  socket_mapreduce:start();
 match_data(_) ->
   "no match".
