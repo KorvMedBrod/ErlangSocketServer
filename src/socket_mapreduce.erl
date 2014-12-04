4f65204375b0ae865f8e5e9b262501d201e64e6c
Johan Hermansson <JohanHermansson@Johans-MacBook-Pro.local>
2014-12-04 14:59:53 +0100

    fixed socket_mapreduce

--- src/socket_mapreduce.erl
+++ src/socket_mapreduce.erl
@@ -42,43 +42,36 @@ mapred(Pid, Bucket) ->
 %get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [{HHash, HV}], NotBiggest, Big) when (V1 < V2) and (V2 > HV) -> get_most_popular_tweets(Startlist, [{Hash2, V2}], [{Hash1, V1} | {HHash, HV} | NotBiggest], Big);
 %get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [{HHash, HV}], NotBiggest, Big) when (V1 < V2) and (V2 < HV) -> get_most_popular_tweets(Startlist, [{HHash, HV}], [{Hash1, V1} | {Hash2, V2} | NotBiggest], Big).
 
-<<<<<<< HEAD:mapreduce.erl
 get_most_popular_tweets([], [Big], NotUsed, List) -> checklist(NotUsed, Big, List);
 get_most_popular_tweets([{Hash1,V1} , {Hash2,V2} | Startlist], [], [], Big)
-  when V1 >= V2 -> get_most_popular_tweets(Startlist,[{Hash1, V1}],[{Hash2, V2}], Big);
+when V1 >= V2 -> get_most_popular_tweets(Startlist,[{Hash1, V1}],[{Hash2, V2}], Big);
 get_most_popular_tweets([{Hash1, V1},{Hash2, V2} | Startlist], [], [], Big)
-=======
-get_most_popular_tweets([{<<>>}], [Big], NotUsed, List) -> checklist(NotUsed, Big, List);
-get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [], [], Big)
-  when V1 >= V2 -> get_most_popular_tweets(Startlist, [{Hash1, V1}], [{Hash2, V2}], Big);
-get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [], [], Big)
->>>>>>> FETCH_HEAD:src/socket_mapreduce.erl
-  when V1 < V2 -> get_most_popular_tweets(Startlist, [{Hash2, V2}], [{Hash1, V1}], Big);
+when V1 < V2 -> get_most_popular_tweets(Startlist, [{Hash2, V2}], [{Hash1, V1}], Big);
 get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [{HHash, HV}], NotBiggest, Big)
-  when (V1 >= V2)
-  and (V1 > HV) ->
-    get_most_popular_tweets(Startlist, [{Hash1, V1}], [{Hash2, V2} , {HHash, HV} | NotBiggest], Big);
+when (V1 >= V2)
+and (V1 > HV) ->
+  get_most_popular_tweets(Startlist, [{Hash1, V1}], [{Hash2, V2} , {HHash, HV} | NotBiggest], Big);
 
-get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [{HHash, HV}], NotBiggest, Big)
+  get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [{HHash, HV}], NotBiggest, Big)
   when (V1 >= V2) and (V1 =< HV) -> get_most_popular_tweets(Startlist, [{HHash, HV}], [{Hash2, V2} , {Hash1, V1} | NotBiggest], Big);
-get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [{HHash, HV}], NotBiggest, Big)
+  get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [{HHash, HV}], NotBiggest, Big)
   when (V1 < V2) and (V2 > HV) -> get_most_popular_tweets(Startlist, [{Hash2, V2}], [{Hash1, V1} , {HHash, HV} | NotBiggest], Big);
-get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [{HHash, HV}], NotBiggest, Big)
+  get_most_popular_tweets([{Hash1, V1} , {Hash2, V2} | Startlist], [{HHash, HV}], NotBiggest, Big)
   when (V1 < V2) and (V2 =< HV) -> get_most_popular_tweets(Startlist, [{HHash, HV}], [{Hash1, V1} , {Hash2, V2} | NotBiggest], Big);
-get_most_popular_tweets([{Hash1, V1}], [{HHash, HV}], NotBiggest, Big)
+  get_most_popular_tweets([{Hash1, V1}], [{HHash, HV}], NotBiggest, Big)
   when (V1 =< HV) -> get_most_popular_tweets([], [{HHash, HV}], [{Hash1, V1} | NotBiggest], Big);
-get_most_popular_tweets([{Hash1, V1}], [{HHash, HV}], NotBiggest, Big)
+  get_most_popular_tweets([{Hash1, V1}], [{HHash, HV}], NotBiggest, Big)
   when (V1 > HV) -> get_most_popular_tweets([], [{Hash1, V1}], [{HHash, HV} | NotBiggest], Big).
 
-checklist(NotUsed, Big, List) ->
-  case check_length(List, 0) of
-    5 -> List;
-    4 -> get_most_popular_tweets(NotUsed, [], [], [Big | List]);
-    3 -> get_most_popular_tweets(NotUsed, [], [], [Big | List]);
-    2 -> get_most_popular_tweets(NotUsed, [], [], [Big | List]);
-    1 -> get_most_popular_tweets(NotUsed, [], [], [Big | List]);
-    0 -> get_most_popular_tweets(NotUsed, [], [], [Big | List])
-  end.
+  checklist(NotUsed, Big, List) ->
+    case check_length(List, 0) of
+      5 -> List;
+      4 -> get_most_popular_tweets(NotUsed, [], [], [Big | List]);
+      3 -> get_most_popular_tweets(NotUsed, [], [], [Big | List]);
+      2 -> get_most_popular_tweets(NotUsed, [], [], [Big | List]);
+      1 -> get_most_popular_tweets(NotUsed, [], [], [Big | List]);
+      0 -> get_most_popular_tweets(NotUsed, [], [], [Big | List])
+    end.
 
-check_length([], N) -> N;
-check_length([_ | Tail], N) -> check_length(Tail, N+1).
+    check_length([], N) -> N;
+    check_length([_ | Tail], N) -> check_length(Tail, N+1).
