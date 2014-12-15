2c33d9d7119fe15872fcae9a7e686aaf7ff86d54
Ludwig Björk <gusbjorklu@student.gu.se>
2014-12-15 15:25:25 +0100

    changed so it won't run in separate thread

--- src/server_handler.erl
+++ src/server_handler.erl
@@ -2,17 +2,18 @@
 
 -author('L Bjork <gusbjorklu@student.gu.se>').
 
--export([stop/0,stopping/0,start/0]).
+-export([stop/0,status/0]).
 
 stop() ->
-  spawn(?MODULE, stopping, []).
-
-stopping() ->
   {ok,ServerName}=inet:gethostname(),
   Conector = "server_socket@",
   Connection = Conector ++ ServerName,
-  %net_kernel:connect_node(Connection).
+  net_kernel:connect_node(Connection).
   rpc:call(list_to_atom(Connection), init, stop, []).
 
-start() ->
-  io:format("testing").
+
+status() ->
+  {ok,ServerName}=inet:gethostname(),
+  Conector = "server_socket@",
+  Connection = Conector ++ ServerName,
+  net_kernel:connect_node(Connection).
